331c0777f32e1df1a88ea928c1ea63a2cfc4e6df
Krzysztof Funkowski <69473574+Funiek@users.noreply.github.com>
2024-04-18 15:04:51 +0200

    zadanie 1 i dodatkowe z sparka zrobione

--- spark/zad1_df.py
+++ spark/zad1_df.py
@@ -7,7 +7,7 @@ spark = SparkSession.builder.appName("Log Processing DF").getOrCreate()
 log_path = "hdfs:///data/sparklogs/huge.log"
 df = spark.read.option("delimiter", "\t").csv(log_path)
 
-df.show()
+# df.show()
 
 record_count = df.count()
 print(f"Record count: {record_count}")
@@ -15,5 +15,8 @@ print(f"Record count: {record_count}")
 filtered_users_count = df.select("_c1").filter((col("_c1") == "bob") | (col("_c1") == "alice")).count()
 print(f"Filtered users count: {filtered_users_count}")
 
-time_sum = df.select(col("_c1").alias("user"),col("_c5").cast("int").alias("time")).filter(col("user") == "bob").groupBy("user").sum("time")
+time_sum = df.select(col("_c1").alias("user"), \
+                     col("_c5").cast("int").alias("time")) \
+                        .filter(col("user") == "bob").groupBy("user").sum("time")
+# print(f"Total time for bob: {time_sum}")
 time_sum.show()
--- spark/zad1_rdd.py
+++ spark/zad1_rdd.py
@@ -1,18 +1,29 @@
 from pyspark.sql import SparkSession
+import time
 
 spark = SparkSession.builder.appName("Log Processing RDD").getOrCreate()
 sc = spark.sparkContext
 
-log_path = "hdfs:///data/sparklogs/small.log"
+log_path = "hdfs:///data/sparklogs/huge.log"
 
-rdd = sc.textFile(log_path)
+rdd = sc.textFile(log_path).cache()
+rdd.count()
+rdd.filter(lambda line: "bob" in line.split("\t")[1] or "alice" in line.split("\t")[1]).count()
+rdd.filter(lambda line: "bob" == line.split("\t")[1]) \
+                  .map(lambda line: int(line.split("\t")[5])) \
+                  .reduce(lambda a, b: a + b)
 
-record_count = rdd.take(2)
-print(f"Record count: {record_count}")
+start_time_record_count = time.time()
+record_count = rdd.count()
+print(f"Record count: {record_count} Execution time: {time.time() - start_time_record_count}")
 
-# filtered_users_count = logs_rdd.filter(lambda line: "bob" in line.split("\t")[1] or "alice" in line.split("\t")[1]).count()
-# print(f"Filtered users count: {filtered_users_count}")
 
-# bob_time_sum = logs_rdd.filter(lambda line: "bob" == line.split("\t")[1]).map(lambda line: int(line.split("\t")[4])).reduce(lambda a, b: a + b)
+start_time_filtered_users_count = time.time()
+filtered_users_count = rdd.filter(lambda line: "bob" in line.split("\t")[1] or "alice" in line.split("\t")[1]).count()
+print(f"Filtered users count: {filtered_users_count} Execution time: {time.time() - start_time_filtered_users_count}")
 
-# print(f"Total time for bob: {bob_time_sum}")
+start_time_bob_time_sum = time.time()
+bob_time_sum = rdd.filter(lambda line: "bob" == line.split("\t")[1]) \
+                  .map(lambda line: int(line.split("\t")[5])) \
+                  .reduce(lambda a, b: a + b)
+print(f"Total time for bob: {bob_time_sum} Execution time: {time.time() - start_time_bob_time_sum}")
